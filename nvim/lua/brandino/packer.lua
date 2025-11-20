c43d7c118e22b64e487f3c5c1b7c532ef01685be
Brandon Carpenter <bjc1269@gmail.com>
2025-11-20 15:06:36 -0700

    added ghub and more fixes

--- nvim/lua/brandino/packer.lua
+++ nvim/lua/brandino/packer.lua
@@ -4,10 +4,14 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
    use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  'nvim-telescope/telescope.nvim', branch = '0.1.x',
 	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+	  requires = { {'nvim-lua/plenary.nvim'} ,
+        {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }}
   }
+
+  use {'github/copilot.vim'}
+
 
   use({
 	  'rose-pine/neovim',
